0de348dc9e2f1a9fd63717c8b4ba32a896a2b5f4
taeho <taeho9421@gmail.com>
2025-04-01 22:41:58 +0900

    20250401

---
+++ app/Models/ProductFile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductFile extends Model
+{
+  use HasFactory;
+  protected $primaryKey = 'product_file_id';
+  protected $table = 'productFile';
+  protected $fillable = [
+    'product_id',
+    'product_file_id',
+    'file_path',
+    'saved_file',
+    'origin_file'
+  ];
+  public $timestamps = false;
+
+}
